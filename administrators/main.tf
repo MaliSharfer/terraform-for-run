424e5bc8cb7d1789057401f37914a569ec46ae01
MaliSharfer <malis@skyvar.co.il>
2024-01-10 09:46:42 +0200

    first commit3

--- administrators/main.tf
+++ administrators/main.tf
@@ -91,8 +91,8 @@ resource "azurerm_function_app" "function_app" {
 
 resource "azurerm_key_vault_access_policy" "example" {
   key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  tenant_id    = data.azurerm_client_config.current_client.tenant_id
+  object_id    = data.azurerm_client_config.current_client.object_id
 
   key_permissions = [
     "Get",
