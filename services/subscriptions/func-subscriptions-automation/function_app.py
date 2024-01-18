9383c79b7e010f2b5aabb94fe5f9718a41a57288
MaliSharfer <malis@skyvar.co.il>
2024-01-18 14:57:08 +0200

    my ci

--- services/subscriptions/func-subscriptions-automation/function_app.py
+++ services/subscriptions/func-subscriptions-automation/function_app.py
@@ -43,6 +43,6 @@ def func_subscriptions_automation(req: func.HttpRequest) -> func.HttpResponse:
         write_to_excel(subscriptions_to_excel)
    
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully work.",
+        "This HTTP triggered function executed successfully work!!!!!!!",
         status_code = 200
     )
