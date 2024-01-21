0b0b95849475f5fbc2d032db0a952b519670656d
MaliSharfer <malis@skyvar.co.il>
2024-01-21 14:18:48 +0200

    apply

--- subscriptions-automation/main.tf
+++ subscriptions-automation/main.tf
@@ -43,20 +43,20 @@ resource "azurerm_storage_account" "vnet_storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # network_rules {
-  #   default_action             = "Deny"
-  #   virtual_network_subnet_ids = [azurerm_subnet.vnet_subnet.id]
-  #   ip_rules                   = ["84.110.136.18"]
-  # }
+  network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = [azurerm_subnet.vnet_subnet.id]
+    ip_rules                   = ["84.110.136.18"]
+  }
 
 }
 
-resource "azurerm_storage_account_network_rules" "network_rules" {
-  storage_account_id    = azurerm_storage_account.vnet_storage_account.id
-  default_action             = "Deny"
-  virtual_network_subnet_ids = [azurerm_subnet.vnet_subnet.id]
-  ip_rules                   = ["84.110.136.18"]
-}
+# resource "azurerm_storage_account_network_rules" "network_rules" {
+#   storage_account_id    = azurerm_storage_account.vnet_storage_account.id
+#   default_action             = "Deny"
+#   virtual_network_subnet_ids = [azurerm_subnet.vnet_subnet.id]
+#   ip_rules                   = ["84.110.136.18"]
+# }
 
 data "azurerm_key_vault" "key_vault" {
   name                = var.key_vault_name
@@ -175,10 +175,10 @@ resource "azurerm_storage_table" "example" {
   storage_account_name = azurerm_storage_account.vnet_storage_account.name
   count = length(var.table_name)
 
-  depends_on = [
-    azurerm_storage_account_network_rules.network_rules
- ]
- 
+#   depends_on = [
+#     azurerm_storage_account_network_rules.network_rules
+#  ]
+
 }
 
 
