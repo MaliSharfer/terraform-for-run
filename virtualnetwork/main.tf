3129e79fdc288b9cdb271c0defb3c2497915a259
MaliSharfer <malis@skyvar.co.il>
2024-01-23 10:21:01 +0200

    networking

--- virtualnetwork/main.tf
+++ virtualnetwork/main.tf
@@ -25,28 +25,30 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example" {
   name                = var.nt_name[count.index]
   address_space       = ["172.16.0.0/12"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example[count.index].location
+  resource_group_name = azurerm_resource_group.example[count.index].name
 
   count = length(var.nt_name)
 }
 
 resource "azurerm_virtual_wan" "example" {
-  name                = "example-vwan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "mal-vwan"
+  resource_group_name = azurerm_resource_group.example[count.index].name
+  location            = azurerm_resource_group.example[count.index].location
+  count = length(var.rg_name)
 }
 
 resource "azurerm_virtual_hub" "example" {
-  name                = "example-hub"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "mal-hub"
+  resource_group_name = azurerm_resource_group.example[count.index].name
+  location            = azurerm_resource_group.example[count.index].location
   virtual_wan_id      = azurerm_virtual_wan.example.id
   address_prefix      = "10.0.1.0/24"
+  count = length(var.rg_name)
 }
 
 resource "azurerm_virtual_hub_connection" "example" {
-  name                      = "example-vhub"
+  name                      = "mal-vhub"
   virtual_hub_id            = azurerm_virtual_hub.example.id
   remote_virtual_network_id = azurerm_virtual_network.example.id
 }
