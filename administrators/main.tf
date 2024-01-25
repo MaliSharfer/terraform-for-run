37fbe806a3d27e178732ad6d4db6ff25de5dbcce
MaliSharfer <malis@skyvar.co.il>
2024-01-25 09:52:18 +0200

    sarle

--- administrators/main.tf
+++ administrators/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_storage_account" "vnet_storage_account" {
 resource "azurerm_storage_account_network_rules" "network_rules" {
   storage_account_id    = azurerm_storage_account.vnet_storage_account.id
   default_action             = "Deny"
-  virtual_network_subnet_ids = [var.vnet_subnet[0].name]
+  virtual_network_subnet_ids = [var.virtual_networks_and_subnets_properties[0].snet_name]
   ip_rules                   = ["84.110.136.18"]
 }
 
