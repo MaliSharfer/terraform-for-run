fdfac14cd1f520d7cad06fd979a8ad992d270888
MaliSharfer <malis@skyvar.co.il>
2024-02-07 09:22:46 +0200

    func-try2

--- services/emails/func-try2/function_app.py
+++ services/emails/func-try2/function_app.py
@@ -31,7 +31,7 @@ def send_email_function(req: func.HttpRequest) -> func.HttpResponse:
         },
         data = email_data,
     ) 
-    logging.info("The email was sent!!!!!5555555555-********")
+    logging.info("The email was sent!!!!!5555555555-********-------------------------------")
     return func.HttpResponse(
         "This HTTP triggered function executed successfully.",
         status_code = 200
