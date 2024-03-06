8550fdef8433fe778f82ed5b0ec7e416e08a959a
MaliSharfer <malis@skyvar.co.il>
2024-03-06 14:16:09 +0200

    tflint

--- main-project/main.tf
+++ main-project/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "functionyvwow"
-    storage_account_name = "functionyvwowdcbbfc"
-    container_name       = "terraformstate-modules"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "functionyvwow"
+  #   storage_account_name = "functionyvwowdcbbfc"
+  #   container_name       = "terraformstate-modules"
+  #   key                  = "terraform.tfstate"
+  # }
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
